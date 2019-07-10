75f9a0a92b3aafa5bb30d22e2485eb9bdcd2eadb
Jirapat11 <52165713+Jirapat11@users.noreply.github.com>
2019-07-10 21:54:40 +0700

    Add files via upload

--- grade.cpp
+++ grade.cpp
@@ -6,14 +6,15 @@ void main()
 	cout << "Enter your score : ";
 	cin >> grade ;
 	cout << "Your grade is : ";
-	if (grade>=80){
+	if (grade<=100&&grade>=90){
 		cout << "A\n";
 	}
-	else if (grade>=70) cout << "B\n";
-	else if (grade>=60) cout << "C\n";
-	else if (grade>=50) cout << "D\n";
-	else {
-		cout << "F\n";
+	else if (grade<=89&&grade>=80) cout << "B\n";
+	else if (grade<=79&&grade>=70) cout << "C\n";
+	else if (grade<=69&&grade>=60) cout << "D\n";
+	else if (grade<=59&&grade>=0) cout << "F\n";
+	else { cout << "ERROR\n" ;
 	}
+	
 }
 
